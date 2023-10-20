346ee1c815a913cfb00e58d30efbd3febacf8fa1
Lucas Beneti <lbeneti@gmail.com>
2023-10-20 18:39:56 -0300

    added the migration to create the table that'll handle the user following artist activity

---
+++ src/database/migrations/20231020121236_create_table_user_follows_artists.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('user_artist_following', (t) => {
+        t.integer('user_id')
+            .references('users.id')
+            .notNullable()
+            .onDelete('CASCADE');
+        t.integer('artist_id')
+            .references('artists.id')
+            .notNullable()
+            .onDelete('CASCADE');
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('user_artist_following');
+}
