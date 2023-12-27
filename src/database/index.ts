83263b06ac35a6e6194ed565a610234ca1caebde
lucasbeneti <lbeneti@gmail.com>
2023-12-26 23:17:21 -0300

    editing the index server to use the correct way of DB connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,14 +2,7 @@ import knex from "knex";
 
 const database = knex({
   client: "pg",
-  connection: `postgresql://postgres:${process.env.DB_PASSWORD}@db.hkxqnzjaporhzwnwmdwm.supabase.co:5432/postgres`,
-  // connection: {
-  //   host: process.env.DB_HOST,
-  //   port: Number(process.env.DB_PORT),
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_NAME,
-  // },
+  connection: process.env.DB_CONNECTION_URL,
 });
 
 export default database;
